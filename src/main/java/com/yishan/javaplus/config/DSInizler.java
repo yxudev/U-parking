b76625280e70e87bbe7896dd26d89699c64c3b94
Yishan Xu <yxu1234@umd.edu>
2018-11-20 12:50:02 -0500

    appconfig

---
+++ src/main/java/com/yishan/javaplus/config/DSInizler.java
@@ -0,0 +1,8 @@
+package com.yishan.javaplus.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DSInizler {
+}
---
+++ src/main/java/com/yishan/javaplus/config/MvcConfig.java
@@ -0,0 +1,7 @@
+package com.yishan.javaplus.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MvcConfig {
+}
