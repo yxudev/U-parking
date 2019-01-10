0214c98f9e7e1f2550619b20510cc40896e2cbdf
Yishan Xu <yxu1234@umd.edu>
2019-01-10 15:38:43 -0500

    secConfig

--- src/main/java/com/yishan/javaplus/repository/ManagerRepository.java
+++ src/main/java/com/yishan/javaplus/repository/ManagerRepository.java
@@ -6,7 +6,7 @@
 //
 //import java.util.List;
 //
-//public interface ManagerRepository extends CrudRepository<User, Long> {
+//        public interface ManagerRepository extends CrudRepository<User, Long> {
 //
 //    List<User> findAll();
 //    @Query
