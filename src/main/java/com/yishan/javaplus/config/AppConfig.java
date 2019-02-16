1d4979f828c0361dec897dec0b630e50f1bbbf35
Yishan Xu <yxu1234@umd.edu>
2019-02-16 14:24:04 -0500

    update config for mock testing

--- src/main/java/com/yishan/javaplus/config/AppConfig.java
+++ src/main/java/com/yishan/javaplus/config/AppConfig.java
@@ -1,8 +1,13 @@
 package com.yishan.javaplus.config;
 
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.yishan.javaplus.service.StorageService;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,6 +16,9 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
+
+import static javax.swing.plaf.basic.BasicHTML.propertyKey;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Configuration
@@ -36,5 +44,13 @@ public class AppConfig {
         PropertiesFactoryBean shareProperties = new PropertiesFactoryBean();
         shareProperties.setLocation(new ClassPathResource("META-INF/share-runtime.properties"));
         return shareProperties;
+    }
+
+    @Bean
+    public StorageService getStorageService(@Autowired @Qualifier("applicationProperties") PropertiesFactoryBean beanFactory) throws IOException {
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(new DefaultAWSCredentialsProviderChain()).build();
+        StorageService storageService = new StorageService(s3Client);
+        storageService.setBucket(beanFactory.getObject().getProperty(propertyKey));
+        return storageService;
     }
 }
