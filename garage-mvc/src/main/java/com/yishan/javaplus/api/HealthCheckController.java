db9d0fea730747f31f875fb025ee671302c3f5a8
Yishan Xu <yxu1234@umd.edu>
2019-05-08 14:23:32 -0400

    healthCheckCOntroller

--- garage-mvc/src/main/java/com/yishan/javaplus/api/HealthCheckController.java
+++ garage-mvc/src/main/java/com/yishan/javaplus/api/HealthCheckController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = {"/api/msg"})
-public class NoController {
+public class HealthCheckController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String noController() {
+    public String healthCheck() {
         return "This application is running okay! ";
     }
 }
