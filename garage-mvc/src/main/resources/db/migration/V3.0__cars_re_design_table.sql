363ab8e8fbb8e4d36f05eb312b66dec6a6fb625c
Yishan Xu <yxu1234@umd.edu>
2019-08-12 16:42:51 -0400

    redesigned table car/plate/garages and schema migration

--- garage-mvc/src/main/resources/db/migration/V3.0__cars_re_design_table.sql
+++ garage-mvc/src/main/resources/db/migration/V3.0__cars_re_design_table.sql
@@ -1,17 +1,24 @@
 CREATE SEQUENCE cars_id_seq;
-create table cars (
-id bigint not null DEFAULT NEXTVAL('cars_id_seq'),
-vin varchar(255) not NULL,
-model varchar(255) not NULL,
-type varchar(255) not NULL,
-garages_id bigint default null,
-plates_id bigint default null,
-primary key (id),
-CONSTRAINT fk_garage,
-FOREIGN KEY (garages_id) REFERENCES garages (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    create table cars (
+        id bigint not null DEFAULT NEXTVAL('cars_id_seq'),
+        vin varchar(255) not NULL,
+        model varchar(255) not NULL,
+        type varchar(255) not NULL,
+        garages_id bigint default null,
+        plates_id bigint default null,
+        primary key (id),
 
-primary key (id),
-CONSTRAINT fk_plates,
-FOREIGN KEY (plates_id) REFERENCES plates (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+        CONSTRAINT fk_plates
+        FOREIGN KEY (plates_id)
+        REFERENCES plates (id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+
+
+        CONSTRAINT fk_garages
+        FOREIGN KEY (garages_id)
+        REFERENCES garages (id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
 );
 ALTER SEQUENCE cars_id_seq OWNED BY cars.id;
