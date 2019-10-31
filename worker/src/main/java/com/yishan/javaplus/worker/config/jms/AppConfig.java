5d00017167751b89b2f8eabb0e83f72fa669fa92
Yishan Xu <yxu.dev@gmail.com>
2019-10-31 14:37:32 -0400

    :apple:Twilio intergation :sparkles:

--- worker/src/main/java/com/yishan/javaplus/worker/config/jms/AppConfig.java
+++ worker/src/main/java/com/yishan/javaplus/worker/config/jms/AppConfig.java
@@ -3,18 +3,50 @@ package com.yishan.javaplus.worker.config.jms;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import com.twilio.Twilio;
+import com.twilio.twiml.voice.Sms;
+import com.yishan.javaplus.worker.service.SmsSender;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Configuration
+@ComponentScan(basePackages = "com.yishan.javaplus", excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX,pattern = "com.yishan.javaplus.api.*"))
 @Profile({"dev", "test", "stage", "prod"})
 public class AppConfig {
+
     @Bean
     public AmazonSQS getAmazonSQS() {
         AmazonSQS amazonSQS = AmazonSQSClientBuilder.standard().withCredentials(new DefaultAWSCredentialsProviderChain()).build();
         return amazonSQS;
-
     }
+
+
+
+        @Autowired
+        private Environment environment;
+
+        private final Logger logger = getLogger(getClass());
+
+        @Value("${queueName}")
+        private String queueName;
+
+        @Bean(name = "application.properties")
+        public PropertiesFactoryBean getDbProperties() {
+            PropertiesFactoryBean bean = new PropertiesFactoryBean();
+            String profile = environment.getActiveProfiles()[0];
+            logger.debug("applicationProperties is " + profile);
+            bean.setLocation(new ClassPathResource("META-INF/env/application-" + profile + ".properties"));
+            return bean;
+        }
+
 }
--- worker/src/main/java/com/yishan/javaplus/worker/service/SmsSender.java
+++ worker/src/main/java/com/yishan/javaplus/worker/service/SmsSender.java
@@ -1,4 +1,44 @@
 package com.yishan.javaplus.worker.service;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+@Service
 public class SmsSender {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Value("#{${$application.properties}['ACCOUNT_SID']}")
+    private String ACCOUNT_SID;
+
+    @Value("#{${$application.properties}['AUTH_TOKEN']}")
+    private String AUTH_TOKEN;
+
+    @Value("#{${$application.properties}['rXNumber']}")
+    private String rXNumber;
+
+    @Value("#{${application.properties}['tXNumber']}")
+    private String tXNumber;
+
+
+    private void smsSender() {
+        logger.debug("Please check your SMS message!");
+        Twilio.init("ACCOUNT_SID", "AUTH_TOKEN");
+        Message message = Message.creator( new PhoneNumber(rXNumber), new PhoneNumber(tXNumber),"Welcome to the system!").create();
+        System.out.println(message.getSid());
+    }
+
+    public static void main(String[] args) {
+        SmsSender smsSender = new SmsSender();
+        smsSender.smsSender();
+    }
 }
